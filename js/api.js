59fb6de26a8ce2b605b6537eee9839b89080b430
Neidson <gaaneidson@gmail.com>
2019-04-11 17:02:10 -0300

    Conexão estabelecida sem range de ano

--- js/api.js
+++ js/api.js
@@ -3,61 +3,60 @@ $(document).ready(function () {
     var publicKey = "84176a028be87e10753e04eb3af73f45"
     var privateKey = "a343cf1548a66d334d41afe694402711d2513dad"
 
-    function getComicData(year) {
-        var ts = Date.now();
-        var hash = CryptoJS.MD5(ts + privateKey + publicKey);
+    var ts = Date.now();
+    var hash = CryptoJS.MD5(ts + privateKey + publicKey);
 
-        var url = 'http://gateway.marvel.com:80/v1/public/comics?limit=20&format=comic&formatType=comic&dateRange=' + year + '-01-01%2C' + year + '-12-31&ts=' + ts + '&apikey=' + publicKey + '&hash=' + hash;
-        console.log('getComicData(' + year + ')');
-        return $.get(url);
-    }
-
-    $(document).ready(function () {
-
+    var url = 'http://gateway.marvel.com:80/v1/public/comics?limit=20&format=comic&formatType=comic&ts=' + ts + '&apikey=' + publicKey + '&hash=' + hash;
+    $.getJSON(url)
+    .done(function (response) {
+        
         var $status = $("#status");
+        
+        $status.html("");
+        var results = response.data.results;
+        var resultsLen = results.length;
+        
+        console.log("res", resultsLen)
 
-        var start = 2019;
-        var end = 2010;
+            for (var i = 0; i < resultsLen; i++) {
+                var comic = results[i];
 
-        var promises = [];
+                var content1 = [comic.title, comic.prices[0].price, comic.thumbnail.path + "." + comic.thumbnail.extension];
 
-        $status.html("<i>Carregando...</i>");
+                $(".title").append("<div class='grid-item'><img src=" + content1[2] +
+                    " class='thumb'><br/>" + content1[0] +
+                    "<br/>" + content1[1] + "</div>")
 
-        for (var x = start; x >= end; x--) {
-            promises.push(getComicData(x));
-        }
-
-        $.when.apply($, promises).done(function () {
-
-            var args = Array.prototype.slice.call(arguments, 0);
-
-            $status.html(""); 
-
-            for (var x = 0; x < args.length; x++) {
-                var year = start - x;
-                console.log("Ano", year);
-                var content = [year];
-
-                var container = $(".year");
-
-                container.append("<h1>" + year + "</h1><br/>");
-
-                var res = args[x][0];
-
-                if (res.code === 200) {
-                    for (var i = 0; i < res.data.results.length; i++) {
-                        var comic = res.data.results[i];
-
-                        var content1 = [comic.title, comic.prices[0].price, comic.thumbnail.path + "." + comic.thumbnail.extension];
-
-                        $(".title").append("<div class='grid-item'><img src=" + content1[2] +
-                            " class='thumb'><br/>" + content1[0] +
-                            "<br/>" + content1[1] + "</div>")
-
-                    }
-                }
             }
         });
+});
 
-    });
-})
+
+// $(function () {
+//     var publicKey = "84176a028be87e10753e04eb3af73f45"
+//     var privateKey = "a343cf1548a66d334d41afe694402711d2513dad"
+
+//     var ts = Date.now();
+//     var hash = CryptoJS.MD5(ts + privateKey + publicKey);
+
+//     var url = 'http://gateway.marvel.com:80/v1/public/comics?limit=20&format=comic&formatType=comic&ts=' + ts + '&apikey=' + publicKey + '&hash=' + hash;
+//     // return $.get(url);
+//     $.getJSON(url)
+//         .done(function (response) {
+//             var results = response.data.results;
+//             var resultsLen = results.length;
+//             var output = '<ul>';
+
+//             for (var i = 0; i < resultsLen; i++) {
+//                 if (results[i].images.length > 0) {
+//                     var imgPath = results[i].images[0].path + '/standard_xlarge.' + results[i].images[0].extension;
+//                     output += '<li><img src="' + imgPath + '"><br>' + results[i].title + '</li>';
+//                 }
+//             }
+//             output += '</ul>'
+//             $('#results').append(output);
+//         });
+
+// });
+
+
