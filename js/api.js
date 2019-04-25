78eb9bef454d52b1425e330ed83ac143f1e0c690
Neidson <gaaneidson@gmail.com>
2019-04-25 15:22:26 -0300

    grid responsive

--- js/api.js
+++ js/api.js
@@ -1,72 +1,59 @@
 $(document).ready(function () {
+    // var teste
+    // function teste(){
+    //     alert($('#inputValor2').val());
+    //     teste = $('#inputValor2').val();
+    //     return $.get(teste);
+    // }
+    // console.log("teste", teste())
 
+    
     var publicKey = "84176a028be87e10753e04eb3af73f45"
     var privateKey = "a343cf1548a66d334d41afe694402711d2513dad"
     var $status = $("#status");
 
     var ts = Date.now();
     var hash = CryptoJS.MD5(ts + privateKey + publicKey);
-
-    var url = 'http://gateway.marvel.com:80/v1/public/characters?limit=20&orderBy=name&name=iron&ts=' + ts + '&apikey=' + publicKey + '&hash=' + hash;
-
-    $status.html("<p>Carregando...</p>");
-
+    
+    var url = 'http://gateway.marvel.com:80/v1/public/characters?limit=10&orderBy=name&ts=' + ts + '&apikey=' + publicKey + '&hash=' + hash;
+    
+    $status.html("<h1>Carregando...</h1>");
+    
     $.getJSON(url)
-        .done(function (response) {
+    .done(function (response) {
+        
+        var results = response.data.results;
+        var resultsLen = results.length;
+        var img = '/img/spide_man.jpg'
+        if (resultsLen <= 0) {
+            $status.html("<h1>Not Fount</h1>");
+        } else {
+            $status.html("");
+            $("footer").removeClass("footer-min");
 
-            var results = response.data.results;
-            var resultsLen = results.length;
-            console.log("result", results.length)
-            var img = '/img/spide_man.jpg'
-            if (resultsLen <= 0) {
-                $status.html("<h1>Not Fount</h1>");
-            } else {
-                $status.html("");
+                var thumb = 0
 
                 for (var i = 0; i < resultsLen; i++) {
                     var comic = results[i];
                     var image = comic.thumbnail.path + "." + comic.thumbnail.extension;
-                    if (image != "http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg"){
-                        
+                    if (image != "http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg") {
+                        thumb =+ i;
+                        console.log("thumb", thumb)
+
                         var content1 = [comic.name, image];
-                        
-                        $(".result").append("<div class='grid-item'>" +
-                        "<img src=" + content1[1] + " class='thumb'>" +
-                        "<p>" + content1[0] + "</p>" +
-                        "</div>")
+
+                        $(".result").append("<article>" +
+                            "<img src=" + content1[1] + " class='article-img' alt=''>" +
+                            "<h1 class='article-title'>" + content1[0] + "</p>" +
+                            "</article>")
                     }
 
+                }
+                if (thumb <= 6) {
+                    $("footer").addClass("footer-min");
                 }
             }
         });
 });
-
-
-// $(function () {
-//     var publicKey = "84176a028be87e10753e04eb3af73f45"
-//     var privateKey = "a343cf1548a66d334d41afe694402711d2513dad"
-
-//     var ts = Date.now();
-//     var hash = CryptoJS.MD5(ts + privateKey + publicKey);
-
-//     var url = 'http://gateway.marvel.com:80/v1/public/comics?limit=20&format=comic&formatType=comic&ts=' + ts + '&apikey=' + publicKey + '&hash=' + hash;
-//     // return $.get(url);
-//     $.getJSON(url)
-//         .done(function (response) {
-//             var results = response.data.results;
-//             var resultsLen = results.length;
-//             var output = '<ul>';
-
-//             for (var i = 0; i < resultsLen; i++) {
-//                 if (results[i].images.length > 0) {
-//                     var imgPath = results[i].images[0].path + '/standard_xlarge.' + results[i].images[0].extension;
-//                     output += '<li><img src="' + imgPath + '"><br>' + results[i].title + '</li>';
-//                 }
-//             }
-//             output += '</ul>'
-//             $('#results').append(output);
-//         });
-
-// });
 
 
