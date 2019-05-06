d88d2aed49cb940955bacc59809c7276a0bf958b
Neidson <gaaneidson@gmail.com>
2019-05-06 15:30:01 -0300

    Inicio inserção campo de pesquisa

--- js/api.js
+++ js/api.js
@@ -7,51 +7,63 @@ $(document).ready(function () {
     // }
     // console.log("teste", teste())
 
+    var form = document.getElementById('formulario');
+    var campo = document.getElementById('campo');
 
-    var publicKey = "84176a028be87e10753e04eb3af73f45"
-    var privateKey = "a343cf1548a66d334d41afe694402711d2513dad"
-    var $status = $("#status");
-    var thumb = 0
+    form.addEventListener('submit', function (e) {
+        // alerta o valor do campo
+        // alert(campo.value);
 
-    var ts = Date.now();
-    var hash = CryptoJS.MD5(ts + privateKey + publicKey);
+        // impede o envio do form
+        e.preventDefault();
 
-    var url = 'http://gateway.marvel.com:80/v1/public/characters?limit=100&orderBy=name&ts=' + ts + '&apikey=' + publicKey + '&hash=' + hash;
 
-    $status.html("<h1>Carregando...</h1>");
+        var publicKey = "84176a028be87e10753e04eb3af73f45"
+        var privateKey = "a343cf1548a66d334d41afe694402711d2513dad"
+        var $status = $("#status");
+        var thumb = 0
 
-    $.getJSON(url)
-        .done(function (response) {
+        var ts = Date.now();
+        var hash = CryptoJS.MD5(ts + privateKey + publicKey);
 
-            var results = response.data.results;
-            var resultsLen = results.length;
-            var img = '/img/spide_man.jpg'
-            if (resultsLen <= 0) {
-                $status.html("<h1>Not Fount</h1>");
-            } else {
-                $status.html("");
-                $("footer").removeClass("footer-min");                
-                
-                for (var i = 0; i < resultsLen; i++) {
-                    var comic = results[i];
-                    var image = comic.thumbnail.path + "." + comic.thumbnail.extension;
-                    if (image != "http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg") {
-                        thumb += 1;
-                        
-                        var content1 = [comic.name, image];
-                        
-                        $(".result").append("<article class='item'>" +
-                            "<img src=" + content1[1] + " class='article-img' alt=''>" +
-                            "<h1 class='article-title'>" + content1[0] + "</p>" +
-                            "</article>")
+        var url = 'http://gateway.marvel.com:80/v1/public/characters?limit=100&orderBy=name&name='+ campo.value +'&ts=' + ts + '&apikey=' + publicKey + '&hash=' + hash;
+
+        $status.html("<h1>Carregando...</h1>");
+
+        $.getJSON(url)
+            .done(function (response) {
+
+                var results = response.data.results;
+                var resultsLen = results.length;
+                var img = '/img/spide_man.jpg'
+                if (resultsLen <= 0) {
+                    $status.html("<h1>Not Fount</h1>");
+                } else {
+                    $status.html("");
+                    $("footer").removeClass("footer-min");
+
+                    for (var i = 0; i < resultsLen; i++) {
+                        var comic = results[i];
+                        var image = comic.thumbnail.path + "." + comic.thumbnail.extension;
+                        if (image != "http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg") {
+                            thumb += 1;
+
+                            var content1 = [comic.name, image];
+
+                            $(".result").append("<article class='item'>" +
+                                "<img src=" + content1[1] + " class='article-img' alt=''>" +
+                                "<h1 class='article-title'>" + content1[0] + "</p>" +
+                                "</article>")
+                        }
+
                     }
+                    if (thumb <= 3) {
+                        $("footer").addClass("footer-min");
+                    }
+                }
+            });
+    });
 
-                }
-                if (thumb <= 3) {
-                    $("footer").addClass("footer-min");
-                }
-            }
-        });
 });
 
 
